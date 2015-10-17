bab2ae7b62fb7a1c54adb7fc709bd3dd5e1bfe57
Anthony Foster <git@af.id.au>
2015-10-17 14:00:53 +1100

    Add index module for destructuring

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+exports.BadRequestError = require('./badrequest');
+exports.ConflictError = require('./conflict');
+exports.ForbiddenError = require('./forbidden');
+exports.InternalError = require('./internal');
+exports.NotFoundError = require('./notfound');
+exports.NotImplementedError = require('./notimplemented');
+exports.UnauthorizedError = require('./unauthorized');
+exports.UnavailableError = require('./unavailable');
